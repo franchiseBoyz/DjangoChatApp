49d5765e86255634a83811e3fbd50ab029dfcdc1
troye <troyegilbert@pm.me>
2022-07-06 01:16:01 +0300

    created account app

---
+++ account/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from account.models import Account
+
+# Register your models here.
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin', 'is_staff')
+    search_fields = ('username', 'email')
+    readonly_fields = ('id', 'date_joined', 'last_login')
+
+    filter_horizonal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account,AccountAdmin)
---
+++ account/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import os
+
+
+# Create your models here
+class MyAccountManager(BaseUserManager):
+    # Function to create new user
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('Email address not found!')
+        if not username:
+            raise ValueError('Username not found')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            )
+        
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    # Function to create superuser
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            password = password,
+            username = username,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+# Function to get the user's profile picture
+def get_profile_image_filepath(self, filename):
+    return 'profile_images/' + str(self.pk) + '/profile_image.png'
+
+# Function to get the default profile picture
+def get_default_profile_image():
+    return 'chatapp/tuvibe.jpg'
+
+class Account(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    profile_image = models.ImageField(max_length=255, upload_to=get_profile_image_filepath, null=True, blank=True, default=get_default_profile_image)
+    hide_email = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    object = MyAccountManager()
+
+    def __str__(self):
+        return self.username
+
+    def get_profile_image_filename(self):
+        return str(self.profile_image)[str(self.profile_image).index('profile_images/' + str(self.pk) + "/"):]
+    
+    # Check the permissions, to keep it simple all admin have ALL permissons
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    # Does this user have permission to view this app? (ALWAYS YES FOR SIMPLICITY)
+    def has_module_perms(self, app_label):
+        return True
