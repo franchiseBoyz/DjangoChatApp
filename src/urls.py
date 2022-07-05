c1072f630ac9461debac36eff28fe98171e1156f
troye <troyegilbert@pm.me>
2022-07-06 01:51:14 +0300

    created registration form

--- src/urls.py
+++ src/urls.py
@@ -4,9 +4,15 @@ from messenger.views import home_screen_view
 from django.conf import settings
 from django.conf.urls.static import static
 
+from account.views import (
+    register_view,
+
+)
+
 urlpatterns = [
     path('', home_screen_view, name='home'),
     path('admin/', admin.site.urls),
+    path('register/', register_view, name="register"),
 ]
 
 if settings.DEBUG:
